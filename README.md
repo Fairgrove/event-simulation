f522147a91d94f3dade393fede77f4759d3f0eca
Fairgrove <fagerlundfr@gmail.com>
2026-05-04 14:39:09 +0200

    event generator is good

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # event-simulation
 Side project for practicing c++ :)
+
+# some notes
+- create processor class with event queue
+- create a blocker when queue is full
+- change the events to be a stream of events with events per second
+- show '''''''real time ''''''' metrics
+- define real time
+
+
+## event stream
+- device struct that uses Event generator class
+- has attribute that determines variance of time between events
+
+increases events per second by increasing amount of 'devices'
+
+
+metrics 
+
+```
+=====================
+events
+       number of devices: 7
+       events per second: xx
+                    loss: xx
+
+server load
+                 servers: 12
+      full server queues: xx
+            total strain: xx%
+=====================
+```
--- main.cpp
+++ main.cpp
@@ -8,63 +8,86 @@
 #include <numeric>
 
 struct Config{
-    int numEvents = 10;
+    int num_events = 1'000;
     int numServers = 2;
     std::vector<int> eventTime = {50'000, 1'000'000};
     
-    double successChance = 0.5;
+    double success_chance = 0.99;
 };
 
 struct Event{
-    int timestamp;
-
-    bool success;
+    int complexity;
+    bool success; // packet loss
+    std::chrono::duration<double> execution_time;
+    std::chrono::duration<double> timestamp;
 };
 
 class EventGenerator {
     private:
+        Config config;
+        std::mt19937 rng;
+
+        std::uniform_int_distribution<uint32_t> complexity_dist;
+        std::bernoulli_distribution success_dist;
+        
+        //std::uniform_int_distribution<uint32_t> server_dist;
+    
     public:
-        EventGenerator(const Config& config) {
-            std::cout << "constructor";
+        EventGenerator(const Config& conf):
+            config(conf),
+            rng(std::random_device{}()),
+            success_dist(config.success_chance),
+            complexity_dist(config.eventTime[0], config.eventTime[1]){
+                std::cout << "constructor" << "\n";
+            }
+        
+        Event next() {
+            Event e;
+            //e.timestamp = std::chrono::high_resolution_clock::now();
+            e.complexity = complexity_dist(rng);
+            e.success = success_dist(rng);
+            return e;
         }
 };
 
-void simulate_work(int duration){
+void simulate_work(int complexity){
     int i = 0;
-    while (i < duration){
+    while (i < complexity){
         i++;
     }
 }
 
 int main(){
     Config config;
+    EventGenerator generator (config);
 
     const auto run_start = std::chrono::high_resolution_clock::now();
 
-    for (int i = 0; i < config.numEvents; i++){
-        const auto start = std::chrono::high_resolution_clock::now();
-        std::mt19937 rng(std::random_device{}());
+    for (int i = 0; i < config.num_events; i++){
+        Event e = generator.next();
 
-        std::uniform_int_distribution<uint32_t> server_dist(1, config.numServers);
-        std::uniform_int_distribution<uint32_t> duration_dist(config.eventTime[0], config.eventTime[1]);
-        std::bernoulli_distribution success_dist(config.successChance);
+        simulate_work(e.complexity);
 
-        simulate_work(duration_dist(rng));
+
+        //const auto start = std::chrono::high_resolution_clock::now();
+
         
-        const auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed = end - start;
-
+        //const auto end = std::chrono::high_resolution_clock::now();
+        //std::chrono::duration<double> elapsed = end - start;
+        
+        /*
         std::cout << "      server: " << server_dist(rng) << "\n";
         std::cout << "     success: " << success_dist(rng) << "\n";
         std::cout << "    duration: " << duration_dist(rng) << "\n";
         std::cout << "        time: " << elapsed.count() << "\n\n";
-        
+        */
     }
     
     const auto run_end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> run_elapsed = run_end - run_start;
 
-    std::cout << "elapsed time: " << run_elapsed.count();
+    std::cout << "elapsed time: " << run_elapsed.count() << "\n";
+    std::cout << "    # events: " << config.num_events << "\n";
 
     return 0;
 }
