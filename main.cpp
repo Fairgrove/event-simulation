6f2b236f674c807bdb50c505017fc2807a901961
Fairgrove <fagerlundfr@gmail.com>
2026-05-13 16:27:23 +0200

    getting closer to fixing things. so far the constructor for event generator and Server is much better

--- main.cpp
+++ main.cpp
@@ -57,29 +57,78 @@ class SafeQueue {
         }
 };
 
-class EventGenerator {
+class Server {
     Config config;
+    
     int id;
-    SafeQueue& event_queue;
-
-    int event_counter = 0;
-
     bool running = false;
+    int total_events_processed = 0;
+    
+    std::mt19937 rng;
+
+    private:
+        void simulate_work(int complexity){
+            int i = 0;
+            while (i < complexity){
+                i++;
+            }
+        }
 
     public:
+        SafeQueue event_queue;
+        
         //constructor with member initializer list
-        EventGenerator(const Config& conf, int generator_id, SafeQueue& eq):
-            config(conf), id(generator_id), event_queue(eq)
+        Server(const Config& conf, int server_id, std::mt19937& random_number_generator):
+            config(conf), id(server_id), rng(random_number_generator)
             {
                 //std::cout << "constructor with member initializer list" << "\n";
                 //std::cout << "   Event Generator: " << id << "\n";
+            }
+        
+        void run(){
+            running = true;
+
+            while (running) {
+                Event e = event_queue.pop();
+                simulate_work(e.complexity);
+                total_events_processed++;
+            }
+        }
+};
+
+class EventGenerator {
+    private:
+        // initial memebers
+        Config config;
+        int id;
+        std::mt19937 rng;
+        std::vector<Server>& servers;
+
+        // defined in constructor body
+        Server& server;
+        SafeQueue& event_queue;
+
+        // metrics
+        int event_counter = 0;
+        bool running = false;
+
+        static Server& selectServer(std::vector<Server>& serv_lst) {
+            return serv_lst[0];
+        }
+
+    public:
+        //constructor with member initializer list
+        EventGenerator(const Config& conf, int generator_id, std::mt19937& random_number_generator, std::vector<Server>& serv_lst):
+            config(conf), id(generator_id), rng(random_number_generator), servers(serv_lst),
+            server(selectServer(serv_lst)),
+            event_queue(server.event_queue)
+            {
             }
         
         void run() {
             running = true;
 
             // create random generators here
-            std::mt19937 rng(std::random_device{}());
             
             std::bernoulli_distribution success_dist; 
             std::uniform_int_distribution<uint32_t> complexity_dist;
@@ -100,60 +149,43 @@ class EventGenerator {
                 
                 // send event to queue
                 event_queue.push(e);
-                event_counter++
+                event_counter++;
                 //std::cout << "hello from generator - " << id << " - queue size : " << event_queue.size()<< "\n";
             }
         }
 };
 
-class Server {
-    Config config;
-    SafeQueue event_queue;
-
-    bool running = false;
-    int id;
-
-    int total_events_received = 0;
-
-    public:
-        //constructor with member initializer list
-        Server(const Config& conf, int server_id):
-            config(conf), id(server_id):
-            {
-                //std::cout << "constructor with member initializer list" << "\n";
-                //std::cout << "   Event Generator: " << id << "\n";
-            }
-        
-        void run(){
-            running = true;
-
-            while (running) {
-                Event e = event_queue.pop();
-
-
-            }
-        }
-};
-
-void simulate_work(int complexity){
-    int i = 0;
-    while (i < complexity){
-        i++;
-    }
-}
-
 int main(){
     Config config;
-    Server server1;
-    server1.run(config);
 
-
-    std::cout << "Firing up " << config.num_generators << " generators" << "\n";
+    std::mt19937 rng(std::random_device{}());
     
+    /*
+     *  Starting Servers
+     */
+    std::cout << "Firing up " << config.num_servers << " Servers" << "\n";
+    std::vector<Server> servers;
+    for (int i = 0; i < config.num_servers; i++){
+        // emplace constructs the generator directly into the vector, which is apparently faster???
+        servers.emplace_back(config, i, &rng); 
+    }
+
+    // generator threads
+    std::vector<std::thread> serverThreads;
+    for (auto& serv : servers){
+        // reference to the i'th instance of generator and 'tells' the thread to run run() from that instance
+        serverThreads.emplace_back(&Server::run, &serv); 
+    }
+    std::cout << "   Done" << "\n\n";
+    
+    /*
+     *  Starting Devics
+     */
+    std::cout << "Firing up " << config.num_generators << " generators" << "\n";
     std::vector<EventGenerator> generators;
     for (int i = 0; i < config.num_generators; i++){
         // emplace constructs the generator directly into the vector, which is apparently faster???
-        generators.emplace_back(config, i, event_queue); 
+        generators.emplace_back(config, i, &rng, &servers); 
     }
 
     // generator threads
@@ -164,10 +196,17 @@ int main(){
     }
     std::cout << "   Done" << "\n";
     
+
+
+
+
+
+
+    /*
     while (true) {
         std::cout << "\r" << "Queue Size : " << event_queue.size() << std::flush;
-
     }
+    */
 
     //handlerThread.join();
     for (auto& t : generatorThreads) t.join();
