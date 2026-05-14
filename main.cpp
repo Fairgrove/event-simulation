8334ca65b604bd5b67e8113503fe739a2ea616dc
Fairgrove <fagerlundfr@gmail.com>
2026-05-14 11:48:54 +0200

    it works but the simulate work thing is weird

--- main.cpp
+++ main.cpp
@@ -10,11 +10,10 @@
 #include <thread>
 
 struct Config{
-    int num_events = 1'000;
-    int num_servers = 2;
-    int num_generators = 1'000;
+    int num_servers = 5;
+    int num_generators = 1000;
 
-    std::vector<int> event_complexity_extremes = {50'000, 1'000'000};
+    std::vector<int> event_complexity_extremes = {1, 2};
     
     double success_chance = 0.99;
 };
@@ -31,42 +30,48 @@ struct Event{
     std::chrono::duration<double> timestamp;
 };
 
-// thread safe queue for events
+// had some help with this one from AI.
+// Cannot create instances if the server class when they have an instance of a class with
+// non-movable attributes `std::mutex` is non-movable
 class SafeQueue {
     std::queue<Event> event_queue;
-    std::mutex mutex;
-    std::condition_variable cv;
+    std::unique_ptr<std::mutex> mutex = std::make_unique<std::mutex>();
+    std::unique_ptr<std::condition_variable> cv = std::make_unique<std::condition_variable>();
 
     public:
         void push(Event e) {
-            std::lock_guard<std::mutex> lock(mutex);
+            std::lock_guard<std::mutex> lock(*mutex);
             event_queue.push(std::move(e));
-            cv.notify_one();  // wake up handler
+            cv->notify_one();
         }
 
         Event pop() {
-            std::unique_lock<std::mutex> lock(mutex);
-            cv.wait(lock, [this]{ return !event_queue.empty(); }); // sleep until data arrives
+            std::unique_lock<std::mutex> lock(*mutex);
+            cv->wait(lock, [this]{ return !event_queue.empty(); });
             Event e = std::move(event_queue.front());
             event_queue.pop();
             return e;
         }
 
         int size() {
+            std::lock_guard<std::mutex> lock(*mutex); // also fix: size() should lock!
             return event_queue.size();
         }
 };
 
 class Server {
-    Config config;
-    
-    int id;
-    bool running = false;
-    int total_events_processed = 0;
-    
-    std::mt19937 rng;
-
     private:
+        // initial members
+        Config config;
+        int id;
+        
+        // defined in constructor body
+        std::mt19937 rng;
+
+        // metrics
+        bool running = false;
+
+        // methods
         void simulate_work(int complexity){
             int i = 0;
             while (i < complexity){
@@ -76,14 +81,13 @@ class Server {
 
     public:
         SafeQueue event_queue;
+        int total_events_processed = 0;
         
         //constructor with member initializer list
-        Server(const Config& conf, int server_id, std::mt19937& random_number_generator):
-            config(conf), id(server_id), rng(random_number_generator)
-            {
-                //std::cout << "constructor with member initializer list" << "\n";
-                //std::cout << "   Event Generator: " << id << "\n";
-            }
+        Server(const Config& conf, int server_id):
+            config(conf), id(server_id), 
+            rng(std::random_device{}())
+            {}
         
         void run(){
             running = true;
@@ -101,10 +105,10 @@ class EventGenerator {
         // initial memebers
         Config config;
         int id;
-        std::mt19937 rng;
         std::vector<Server>& servers;
 
         // defined in constructor body
+        std::mt19937 rng;
         Server& server;
         SafeQueue& event_queue;
 
@@ -112,18 +116,20 @@ class EventGenerator {
         int event_counter = 0;
         bool running = false;
 
-        static Server& selectServer(std::vector<Server>& serv_lst) {
-            return serv_lst[0];
+        // methods
+        Server& selectServer(std::vector<Server>& serv_lst) {
+            std::uniform_int_distribution<> server_id(0, config.num_servers-1);
+            return serv_lst[server_id(rng)];
         }
 
     public:
         //constructor with member initializer list
-        EventGenerator(const Config& conf, int generator_id, std::mt19937& random_number_generator, std::vector<Server>& serv_lst):
-            config(conf), id(generator_id), rng(random_number_generator), servers(serv_lst),
+        EventGenerator(const Config& conf, int generator_id, std::vector<Server>& serv_lst):
+            config(conf), id(generator_id), servers(serv_lst),
+            rng(std::random_device{}()),
             server(selectServer(serv_lst)),
             event_queue(server.event_queue)
-            {
-            }
+            {}
         
         void run() {
             running = true;
@@ -160,18 +166,23 @@ int main(){
 
     std::mt19937 rng(std::random_device{}());
     
+    std::vector<Server> servers;
+    std::vector<EventGenerator> generators;
+    
+    std::vector<std::thread> serverThreads;
+    std::vector<std::thread> generatorThreads;
+
     /*
      *  Starting Servers
      */
+    
     std::cout << "Firing up " << config.num_servers << " Servers" << "\n";
-    std::vector<Server> servers;
     for (int i = 0; i < config.num_servers; i++){
         // emplace constructs the generator directly into the vector, which is apparently faster???
-        servers.emplace_back(config, i, &rng); 
+        servers.emplace_back(config, i); 
     }
 
     // generator threads
-    std::vector<std::thread> serverThreads;
     for (auto& serv : servers){
         // reference to the i'th instance of generator and 'tells' the thread to run run() from that instance
         serverThreads.emplace_back(&Server::run, &serv); 
@@ -182,37 +193,36 @@ int main(){
      *  Starting Devics
      */
     std::cout << "Firing up " << config.num_generators << " generators" << "\n";
-    std::vector<EventGenerator> generators;
     for (int i = 0; i < config.num_generators; i++){
         // emplace constructs the generator directly into the vector, which is apparently faster???
-        generators.emplace_back(config, i, &rng, &servers); 
+        generators.emplace_back(config, i, servers); 
     }
 
     // generator threads
-    std::vector<std::thread> generatorThreads;
     for (auto& gen : generators){
         // reference to the i'th instance of generator and 'tells' the thread to run run() from that instance
         generatorThreads.emplace_back(&EventGenerator::run, &gen); 
     }
     std::cout << "   Done" << "\n";
     
-
-
-
-
-
-
-    /*
     while (true) {
-        std::cout << "\r" << "Queue Size : " << event_queue.size() << std::flush;
-    }
-    */
+        // Print your lines
+        for (int i = 0; i < config.num_servers; i++){
+            std::cout << "Server" << i << "queue size: " << servers[i].event_queue.size() << "  (" << servers[i].total_events_processed << ")\n";
+        }
+        std::cout << "\n";
 
-    //handlerThread.join();
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+        // Move cursor back up N lines
+        for (int i = 0; i < config.num_servers+1 ; i++) {
+            std::cout << "\033[A"  // move up one line
+                      << "\033[2K"; // clear the line
+        }
+    }
+
     for (auto& t : generatorThreads) t.join();
 
-    unsigned int threads = std::thread::hardware_concurrency();
-    std::cout << "threads: " << threads << "\n";
     return 0;
 }
 
@@ -223,4 +233,7 @@ int main(){
 
     std::cout << "elapsed time: " << run_elapsed.count() << "\n";
     std::cout << "    # events: " << config.num_events << "\n";
+    
+    unsigned int threads = std::thread::hardware_concurrency();
+    std::cout << "threads: " << threads << "\n";
 */
